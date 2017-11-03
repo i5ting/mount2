34c18cf99c23b1b31185fd96295b381e771c7d07
i5ting <i5ting@126.com>
2017-11-03 20:33:40 +0800

    lazycommmit, please ignore

---
+++ app.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const Koa = require('koa')
+const app = new Koa()
+ 
+const index = require('./routes/index')
+const users = require('./routes/users')
+ 
+app.use(require('koa-static')(__dirname + '/public'))
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+const render = require('koa-art-template');
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  extname: '.art',
+  debug: process.env.NODE_ENV !== 'production'
+});
+
+app.listen(3001)
+module.exports = app
