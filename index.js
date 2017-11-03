554b6acc2be49d1b456dbee0d54c00b1b20b84b8
i5ting <i5ting@126.com>
2017-11-03 20:27:03 +0800

    lazycommmit, please ignore

---
+++ index.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var dirw = require('dirw');
+
+dirw.dir('.', function(dir_path, dir_name){
+  if(dir_name == 'bin' || dir_name == '.bin'){
+    return;
+  }
+  
+  console.log(dir_path);
+  console.log(dir_name);
+});
